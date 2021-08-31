24162b53089be7790c1c38bb66be9ab783cfd092
ckallcoud@foxmail.com <19990205>
2021-08-31 14:38:39 +0800

    修改ding模块

---
+++ push/ding.go
@@ -0,0 +1,89 @@
+package push
+
+import (
+	"errors"
+	"gck/push/ding"
+)
+
+var (
+	dingTalk = make(map[string]*ding.DingTalk)
+)
+
+type DingTalk struct {
+	name string
+}
+
+type dingPublicMessageBody struct {
+	context string
+	opt     []ding.AtOption
+}
+
+//
+type dingMarkdown struct {
+	name  string
+	title string
+	dingPublicMessageBody
+}
+
+//
+type dingText struct {
+	name string
+	dingPublicMessageBody
+}
+
+/**
+ * @auth: kuncheng
+ * @Date: 2021/8/30
+ */
+//关键词初始化
+//tokens 可以发送给多个群
+//key 每个群的多个关键字验证
+func InitKeyWordDing(name, tokens string, title string) {
+	dingTalk[name] = ding.InitDingTalk(tokens, title)
+}
+
+//签名规则
+func InitSignDing(name, token string, secret string) {
+	dingTalk[name] = ding.InitDingTalkWithSecret(token, secret)
+}
+
+func Ding(name string) *DingTalk {
+	return &DingTalk{
+		name: name,
+	}
+}
+
+//发送Markdown消息
+func (ding *DingTalk) SendMarkdown(title, context string, opt ...ding.AtOption) *dingMarkdown {
+	initMarkDown := &dingMarkdown{
+		name:  ding.name,
+		title: title,
+	}
+	initMarkDown.context = context
+	initMarkDown.opt = opt
+	return initMarkDown
+}
+
+//发送Text消息
+func (ding *DingTalk) SendText(context string, opt ...ding.AtOption) *dingText {
+	initText := &dingText{
+		name: ding.name,
+	}
+	initText.context = context
+	initText.opt = opt
+	return initText
+}
+
+func (ding *dingText) Send() error {
+	if talk, ok := dingTalk[ding.name]; ok {
+		return talk.SendTextMessage(ding.context, ding.opt...)
+	}
+	return errors.New("类型错误")
+}
+
+func (ding *dingMarkdown) Send() error {
+	if talk, ok := dingTalk[ding.name]; ok {
+		return talk.SendMarkDownMessage(ding.title, ding.context, ding.opt...)
+	}
+	return errors.New("类型错误")
+}
---
+++ utils/http.go
@@ -0,0 +1,6 @@
+package utils
+
+/**
+ * @auth: kuncheng
+ * @Date: 2021/8/31
+ */
